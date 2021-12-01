68761ad08c6665d403fe01e97b86c7b2eb057b67
kmaasrud <km@aasrud.com>
2021-12-01 09:41:29 +0100

    Improved day 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,41 +1,53 @@
 use std::fs::File;
 use std::io::Read;
 
+fn a(nums: Vec<usize>) -> usize {
+    nums
+        .iter()
+        .fold((0, usize::MAX), |(sum, prev), next| {
+            (if prev < *next { sum + 1 } else { sum }, *next)
+        }).0
+}
+
+fn b(nums: Vec<usize>) -> usize {
+    let mut nums = nums.iter();
+    let a = nums.next().unwrap();
+    let b = nums.next().unwrap();
+    let c = nums.next().unwrap();
+    nums
+        .fold((0, a, b, c), |(sum, a, b, c), d| {
+            if (a + b + c) < (b + c + d) {
+                (sum + 1, b, c, d)
+            } else {
+                (sum, b, c, d)
+            }
+        }).0
+}
+
 fn main () {
+    let nums = load_lines("inputs/day01.txt");
+
+    println!("First answer: {}", a(nums.clone()));
+
+    println!("Second answer: {}", b(nums));
+}
+
+fn load_lines(path: &str) -> Vec<usize> {
     let mut buf = String::new();
-    File::open("inputs/day01.txt")
+    File::open(path)
         .unwrap()
         .read_to_string(&mut buf)
         .unwrap();
 
-    let list = buf
+    buf
         .lines()
-        .filter_map(|line| line.parse::<usize>().ok());
+        .filter_map(|line| line.parse::<usize>().ok())
+        .collect()
+}
 
-    let answer = list
-        .clone()
-        .zip(list.clone().skip(1))
-        .fold(0, |acc, (a, b)| {
-            if a < b {
-                acc + 1
-            } else {
-                acc
-            }
-        });
-
-    println!("First answer: {}", answer);
-
-    let answer = list.clone()
-        .zip(list.clone().skip(1))
-        .zip(list.clone().skip(2))
-        .zip(list.skip(3))
-        .fold(0, |acc, (((a, b), c), d)| {
-            if (a + b + c) < (b + c + d) {
-                acc + 1
-            } else {
-                acc
-            }
-        });
-
-    println!("Second answer: {}", answer);
+#[test]
+fn test() {
+    let nums = load_lines("inputs/day01_test.txt");
+    assert_eq!(a(nums.clone()), 7);
+    assert_eq!(b(nums), 5);
 }
