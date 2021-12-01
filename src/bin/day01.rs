dd5db19c3c3eed8961036638327b6fe214566027
kmaasrud <km@aasrud.com>
2021-12-01 08:13:30 +0100

    Solved day 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,3 +1,41 @@
-fn main () {
+use std::fs::File;
+use std::io::Read;
 
+fn main () {
+    let mut buf = String::new();
+    File::open("inputs/day01.txt")
+        .unwrap()
+        .read_to_string(&mut buf)
+        .unwrap();
+
+    let list = buf
+        .lines()
+        .filter_map(|line| line.parse::<usize>().ok());
+
+    let answer = list
+        .clone()
+        .zip(list.clone().skip(1))
+        .fold(0, |acc, (a, b)| {
+            if a < b {
+                acc + 1
+            } else {
+                acc
+            }
+        });
+
+    println!("First answer: {}", answer);
+
+    let answer = list.clone()
+        .zip(list.clone().skip(1))
+        .zip(list.clone().skip(2))
+        .zip(list.skip(3))
+        .fold(0, |acc, (((a, b), c), d)| {
+            if (a + b + c) < (b + c + d) {
+                acc + 1
+            } else {
+                acc
+            }
+        });
+
+    println!("Second answer: {}", answer);
 }
