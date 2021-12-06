2f093539dc0d56662b8f04be8ddda1e1c5ab5216
kmaasrud <km@aasrud.com>
2021-12-06 10:52:54 +0100

    Cleaned syntax

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,15 +1,12 @@
-fn a(fish: &[usize], days: usize) -> usize {
+use std::iter::repeat;
+
+fn how_many_fish(fish: &[usize], days: usize) -> usize {
     let mut fish = fish.to_owned();
     for _ in 0..days {
-        let new_fish_iter = std::iter::repeat(8).take(fish.iter().filter(|x| **x == 0).count());
+        let new_fish_iter = repeat(8).take(fish.iter().filter(|x| **x == 0).count());
 
-        fish.iter_mut().for_each(|fish| {
-            *fish = if *fish == 0 {
-                6
-            } else {
-                *fish - 1
-            }
-        });
+        fish.iter_mut()
+            .for_each(|fish| *fish = if *fish == 0 { 6 } else { *fish - 1 });
 
         fish.extend(new_fish_iter);
     }
@@ -19,7 +16,7 @@ fn a(fish: &[usize], days: usize) -> usize {
 fn main() {
     let fish = load_lanternfish("inputs/day06.txt");
 
-    println!("First answer: {}", a(&fish, 80));
+    println!("First answer: {}", how_many_fish(&fish, 80));
 }
 
 fn load_lanternfish(path: &str) -> Vec<usize> {
@@ -33,7 +30,8 @@ fn load_lanternfish(path: &str) -> Vec<usize> {
 #[test]
 fn test() {
     let fish = load_lanternfish("inputs/day06_test.txt");
-    
-    assert_eq!(a(&fish, 18), 26);
-    assert_eq!(a(&fish, 80), 5934);
+
+    assert_eq!(how_many_fish(&fish, 18), 26);
+    assert_eq!(how_many_fish(&fish, 80), 5934);
+    // assert_eq!(how_many_fish(&fish, 256), 26984457539);
 }
