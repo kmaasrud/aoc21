f11255d556000ff55c9520c8470400f1eb6b3f62
kmaasrud <km@aasrud.com>
2021-12-06 11:01:41 +0100

    Solved day 6b

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -13,10 +13,26 @@ fn how_many_fish(fish: &[usize], days: usize) -> usize {
     fish.len()
 }
 
+fn how_many_fish_faster(fish: &[usize], days: usize) -> usize {
+    // Track population as an array indexed by the age
+    let mut pop = [0; 9];
+    for age in fish {
+        pop[*age] += 1;
+    }
+
+    for _ in 0..days {
+        pop.rotate_left(1);
+        pop[6] += pop[8];
+    }
+
+    pop.iter().sum()
+}
+
 fn main() {
     let fish = load_lanternfish("inputs/day06.txt");
 
     println!("First answer: {}", how_many_fish(&fish, 80));
+    println!("Second answer: {}", how_many_fish_faster(&fish, 256));
 }
 
 fn load_lanternfish(path: &str) -> Vec<usize> {
@@ -33,5 +49,6 @@ fn test() {
 
     assert_eq!(how_many_fish(&fish, 18), 26);
     assert_eq!(how_many_fish(&fish, 80), 5934);
-    // assert_eq!(how_many_fish(&fish, 256), 26984457539);
+
+    assert_eq!(how_many_fish_faster(&fish, 256), 26984457539);
 }
