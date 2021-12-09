cb2f82320ceced95ce7c2060ab8d30a17bfd35f9
kmaasrud <km@aasrud.com>
2021-12-09 10:38:03 +0100

    Simplified day 9a

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -1,25 +1,12 @@
 fn a(map: &[Vec<usize>]) -> usize {
     let mut sum = 0;
-    for y in 0..map.len() {
-        for x in 0..map[0].len() {
-            let points = match (y,x) {
-                (0, 0) => vec![(y+1, x), (y, x+1)],
-                (0, _) => vec![(y+1, x), (y, x+1), (y, x-1)],
-                (_, 0) => vec![(y+1, x), (y, x+1), (y-1, x)],
-                _ => vec![(y+1, x), (y, x+1), (y-1, x), (y, x-1)],
-            };
-
-            let adjacent: Vec<&usize> = points
-                .iter()
-                .filter_map(|(y, x)| if let Some(row) = map.get(*y) {
-                    row.get(*x)
-                } else {
-                    None
-                })
-                .collect();
-
+    let maxx = map[0].len() - 1;
+    let maxy = map.len() - 1;
+    for y in 1..maxy {
+        for x in 1..maxx {
             let this = map[y][x];
-            if adjacent.iter().all(|x| **x > this) {
+            let surrounding = [(y+1, x), (y, x+1), (y-1, x), (y, x-1)];
+            if surrounding.iter().all(|(y, x)| map[*y][*x] > this) {
                 sum += 1 + this;
             }
         }
@@ -34,13 +21,21 @@ fn main() {
 }
 
 fn load_map(path: &str) -> Vec<Vec<usize>> {
-    std::fs::read_to_string(path)
+    let mut map: Vec<Vec<usize>> = std::fs::read_to_string(path)
         .unwrap()
         .lines()
         .map(|line| {
             line.chars().map(|c| c.to_string().parse::<usize>().unwrap()).collect()
         })
-        .collect()
+        .collect();
+
+    // Surround with 9's
+    map = [vec![vec![9; map[0].len()]], map].concat();
+    map.iter_mut().for_each(|row| *row = [vec![9], row.to_vec()].concat());
+    map.iter_mut().for_each(|row| row.push(9));
+    map.push(vec![9;map[0].len()]);
+
+    map
 }
 
 #[test]
