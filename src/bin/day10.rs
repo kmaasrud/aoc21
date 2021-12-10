caddcd4f8892175868699658173edff2b45669ba
kmaasrud <km@aasrud.com>
2021-12-10 08:12:15 +0100

    Solved day 10a

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,1 +1,58 @@
-fn main() {}
+fn a(lines: &[String]) -> usize {
+    let mut sum = 0;
+    for line in lines {
+        let mut buf = Vec::new();
+        for c in line.chars() {
+            match c {
+                '(' | '[' | '{' | '<' => buf.push(c),
+                ')' => {
+                    if buf.pop() != Some('(') {
+                        sum += 3;
+                        break
+                    }
+                }
+                ']' => {
+                    if buf.pop() != Some('[') {
+                        sum += 57;
+                        break
+                    }
+                }
+                '}' => {
+                    if buf.pop() != Some('{') {
+                        sum += 1197;
+                        break
+                    }
+                }
+                '>' => {
+                    if buf.pop() != Some('<') {
+                        sum += 25137;
+                        break
+                    }
+                }
+                _ => println!("Invalid char: {}", c),
+            }
+        }
+    }
+    sum
+}
+
+fn main() {
+    let lines = load_lines("inputs/day10.txt");
+
+    println!("First answer: {}", a(&lines));
+}
+
+fn load_lines(path: &str) -> Vec<String> {
+    std::fs::read_to_string(path)
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect()
+}
+
+#[test]
+fn test() {
+    let lines = load_lines("inputs/day10_test.txt");
+
+    assert_eq!(a(&lines), 26397);
+}
